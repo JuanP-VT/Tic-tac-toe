5671df8b2f4f9a2e3858003e28dd36406c8be6b6
PB-vt <cv.juanp@gmail.com>
2022-03-08 12:46:39 -0700

    Now the gameboard is creating as an object insted of raw html elements

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const gameBoard = (() =>{
-    const gameBoardGrid = []
-    function createGrid(status,element){
+    //Creating the gameboard
+    function gridFactory(element,status,owner){
         return{
+            element,
             status,
-            element
+            owner,
         }
     }
+    const gameBoardGrid =[];
     for (let index = 0; index < 9; index++) {
-        const selector = 'n'+index
-        element = document.getElementById(selector)
-        const gridd = createGrid('Available',element,selector)
-        gameBoardGrid.push(gridd)
+        const divField = document.createElement('div')
+        divField.id =(index)
+        divField.classList.add('grid')
+        const objField = gridFactory(divField,'Available','noOne')
+        gameBoardGrid.push(objField)
     }
+//Establishing players
     function createPlayer (name , marker) {
         return{
             name,
@@ -23,6 +27,7 @@ const gameBoard = (() =>{
     }
 const playerOne = createPlayer('Juan P', 'O');
 const playerTwo = createPlayer('Computer','X');
+    
 
 return {
 gameBoardGrid,
@@ -31,6 +36,9 @@ playerTwo,
 };
 
 })();
+
+
+
 
 
 
